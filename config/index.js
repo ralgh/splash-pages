ce2455f07808a0be2044b417fc5e3010f7210398
Philip Harrison <philip@mailharrison.com>
2015-04-22 15:54:19 +0100

    Refactor config

--- config/index.js
+++ config/index.js
@@ -1,4 +1,16 @@
-// Load the appropriate config
-var env = './' + (process.env.NODE_ENV || 'development');
-var config = require(env);
+import path from 'path';
+import _ from 'lodash';
+
+import shared from '../config/shared';
+
+var env = process.env.NODE_ENV || 'development';
+var allowedEnvs = ['production', 'development'];
+
+if (!_.contains(allowedEnvs, env)) {
+  throw new Error(`env not allwed ${env}, [${allowedEnvs}]`);
+}
+
+var config = require(path.join(__dirname, env));
+config = _.merge({}, shared, config);
+
 export default config;
