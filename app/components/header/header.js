7811f5d9efada17e5e0b2ea374ae126e18c69b5e
Iain Nash <iain@gocardless.com>
2015-04-23 14:43:20 +0100

    Updated header

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -9,8 +9,8 @@ class Header extends React.Component {
       <div className='header'>
         <h2>GoCardless</h2>
         <ul className='nav'>
-          <li><LinkExists to='Home'><IntlLink to='Home' message='Home.link'>Home</IntlLink></LinkExists></li>
-          <li><LinkExists to='About'><IntlLink to='About' message='About.link'>About</IntlLink></LinkExists></li>
+          <li><LinkExists to='Home'><IntlLink to='Home' message='Home.link' /></LinkExists></li>
+          <li><LinkExists to='About'><IntlLink to='About' message='About.link' /></LinkExists></li>
         </ul>
       </div>
 
