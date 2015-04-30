126260c7925bf22d0cbb42873cae0d567e2ba481
Iain Nash <iain@gocardless.com>
2015-04-30 17:40:42 +0100

    adding autorestart script fixes

--- app/pages/stories/stories.js
+++ app/pages/stories/stories.js
@@ -37,7 +37,7 @@ export default class Stories extends React.Component {
           <div className='u-padding-Vxxl u-relative u-margin-Bxxl'>
             <div className='u-padding-Bl'>
               <div className='stories-spotlight grid u-margin-Hn u-size-full'>
-                <a href='/stories/has-bean-coffee' className='grid__cell u-text-center u-size-1of2 u-link-clean u-padding-Rxs u-padding-Ln'>
+                <Link to='story' params={{storyId: 'has-bean-coffee'}} className='grid__cell u-text-center u-size-1of2 u-link-clean u-padding-Rxs u-padding-Ln'>
                   <div className='stories-spotlight__item stories-spotlight__item--has-bean-coffee
                   u-relative u-padding-Vl'>
                     <div className='stories-spotlight__item-text u-margin-Vl'>
@@ -50,8 +50,8 @@ export default class Stories extends React.Component {
                     </div>
                   </div>
                   <div className='stories-spotlight__btn btn u-size-full'>Read the case study</div>
-                </a>
-                <a href='/stories/momentum' className='grid__cell u-text-center u-size-1of2 u-link-clean u-padding-Lxs u-padding-Rn'>
+                </Link>
+                <Link to='story' params={{storyId: 'momentum'}} className='grid__cell u-text-center u-size-1of2 u-link-clean u-padding-Lxs u-padding-Rn'>
                   <div className='stories-spotlight__item stories-spotlight__item--crossfit-hackney u-relative u-padding-Vl'>
                     <div className='stories-spotlight__item-text u-margin-Vl'>
                       <p className='u-padding-Al u-padding-Bn u-text-heading u-color-invert u-text-m'>
@@ -63,7 +63,7 @@ export default class Stories extends React.Component {
                     </div>
                   </div>
                   <div className='stories-spotlight__btn btn u-size-full'>Read the case study</div>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
