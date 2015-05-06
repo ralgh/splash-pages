4ac3cc155c6cfd677267bd36e268e3f1dada10db
Philip Harrison <philip@mailharrison.com>
2015-05-06 17:34:12 +0100

    Add fr-be

--- app/pages/security/security.fr.js
+++ app/pages/security/security.fr.js
@@ -7,7 +7,7 @@ export default class SecurityFr extends React.Component {
 
   render() {
     return (
-      <Translation locales='fr-FR'>
+      <Translation locales={['fr-FR', 'fr-BE']}>
         <div className='site-container u-padding-Vxxl'>
           <div className='grid u-center u-padding-Vxm'>
             <div className='grid__cell u-text-center u-size-1of2 u-padding-Vxl'>
