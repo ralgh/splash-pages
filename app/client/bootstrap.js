8b5bd61b7d9b94ff345745f0c1ba0a1618138f93
Iain Nash <iain@gocardless.com>
2015-05-11 14:55:24 +0100

    Linting

--- app/client/bootstrap.js
+++ app/client/bootstrap.js
@@ -26,7 +26,7 @@ function renderApp() {
 
     React.render(<Handler {...stateProps} />, mountNode, () => {
       console.log('App has been mounted. Logging pageview.');
-      dataLayer.push({'event': 'pageview', 'title': document.title, 'virtualUrl': state.pathname});
+      window.dataLayer.push({event: 'pageview', title: document.title, virtualUrl: state.pathname});
     });
   });
 }
