ea9661199298b4a66bdd828e5392b276c565e213
Ken-Lo <ken@kenstechtips.com>
2015-05-18 16:28:57 +0100

    Update homepage, pricing and security page

--- app/pages/pricing/pricing.en-GB.js
+++ app/pages/pricing/pricing.en-GB.js
@@ -3,17 +3,49 @@ import Translation from '../../components/translation/translation';
 import Message from '../../components/message/message';
 import Link from '../../components/link/link';
 
-export default class PricingEn extends React.Component {
-  displayName = 'PricingEn'
+export default class PricingEnGb extends React.Component {
+  displayName = 'PricingEnGb'
 
   render() {
     return (
-      <Translation locales='en-EU'>
+      <Translation locales='en-GB'>
         <div className='page-hero--pricing page-hero'>
           <div className='site-container'>
             <div className='grid pricing-options u-center u-padding-Bxl'>
-              <div className='grid__cell u-padding-Vxl u-padding-Rxs'>
+              <div className='grid__cell u-size-1of2 u-padding-Vxl u-padding-Rxs'>
                 <h1 className='u-text-heading-light u-text-center u-color-invert u-text-l u-padding-Bs'>GoCardless</h1>
+                <div className='u-relative u-background-primary u-padding-Vxl'>
+                  <div className='pricing-options__badge'>
+                    <img src='/images/pricing/most-popular-badge@2x.png' alt='most popular' />
+                  </div>
+                  <h2 className='u-text-heading-light u-text-center u-color-invert u-text-xl'>
+                    <Message message='pricing.per_transaction_amount_normal' />
+                  </h2>
+                  <p className='u-text-heading-light u-text-center u-color-invert u-text-s u-margin-Txxs'>
+                    Per transaction, capped at <Message message="pricing.cost_cap" />
+                  </p>
+                </div>
+                <ul className='pricing-options__list'>
+                  <li className='pricing-options__list-item u-color-heading'>
+                    <b>Perfect for small to medium sized businesses</b>
+                  </li>
+                  <li className='pricing-options__list-item'>
+                    No setup costs, hidden fees, or commitments</li>
+                  <li className='pricing-options__list-item'>
+                    Scale pricing available (<a href='#scale-pricing-container'>?</a>)
+                  </li>
+                  <li className='pricing-options__list-item'>
+                    <Link to='features'>Find out more about GoCardless</Link>
+                  </li>
+                  <Translation locales='en-GB'>
+                    <li className='pricing-options__list-button'>
+                      <a href='/merchants/new' className='btn u-size-full'>Sign up today</a>
+                    </li>
+                  </Translation>
+                </ul>
+              </div>
+              <div className='grid__cell u-size-1of2 u-padding-Vxl u-padding-Rxs'>
+                <h1 className='u-text-heading-light u-text-center u-color-invert u-text-l u-padding-Bs'>Pro</h1>
                 <div className='u-background-dark-gray-darken u-padding-Vxl'>
                   <h2 className='u-text-heading-light u-text-center u-color-invert u-text-xl'>
                     <Message message='pricing.pro_cost_per_transaction' />
@@ -56,8 +88,10 @@ export default class PricingEn extends React.Component {
                 </b>
               </div>
               <p className='u-color-p'>
-                No, we only charge you the <Message message='pricing.pro_monthly_fee' /> monthly fee and a fee for each transaction.
+                No, there are no monthly charges or hidden fees of any kind.
                 We never charge for chargebacks or payment failures.
+                All you'll ever pay is <Message message='pricing.per_transaction_amount_normal' /> of the transaction amount,
+                up to a maximum of <Message message="pricing.cost_cap" />.
               </p>
             </div>
             <div className='grid__cell u-size-1of2 u-padding-Bxxl u-padding-Lm'>
@@ -65,16 +99,18 @@ export default class PricingEn extends React.Component {
                 <b>Do you offer discounts for large volume?</b>
               </div>
               <p className='u-color-p'>
-                Yes. Our transaction fees decrease from 1,000 payments per month.
+                Yes. Discounts for high volume are built into the pricing of GoCardless Pro,
+                and scale pricing is available on request for users of regular GoCardless that take over 250 payments per month.
                 To find out more, please <Link to='contact_sales' className='u-link-color-p u-text-underline'>contact sales</Link>.
               </p>
             </div>
             <div className='grid__cell u-size-1of2 u-padding-Bm u-padding-Rm'>
               <div className='u-color-heading'>
-                <b>Are there any implementation costs?</b>
+                <b>What does ‘capped at <Message message="pricing.cost_cap" />’ mean?</b>
               </div>
               <p className='u-color-p'>
-                No, you won't pay to implement to GoCardless and our support is free.
+                You'll never pay more than <Message message="pricing.cost_cap" /> per transaction
+                even if the transaction value is greater than <Message message='pricing.normal_capped_at_amount' />.
               </p>
             </div>
             <div className='grid__cell u-size-1of2 u-padding-Bm u-padding-Lm'>
@@ -91,8 +127,10 @@ export default class PricingEn extends React.Component {
         <div className='site-container u-text-center u-padding-Tm u-padding-Bxxl'>
           <div className='u-padding-Vxl'>
             <h2 className='u-text-heading u-text-l u-color-heading u-text-light'>Got any questions?</h2>
-            <p className='u-color-p u-margin-Ts'>Speak with one of our payments experts on <Message message='phone_full' /></p>
-            <Link to='contact_sales' query={{ s: 'pricing' }} className='btn btn--hollow u-margin-Tm'>Contact sales</Link>
+            <p className='u-color-p u-margin-Ts'>Contact support on <Message message='phone_full' /></p>
+            <Translation locales='en-GB'>
+              <a href='/merchants/new/' className='btn btn--hollow u-margin-Tm'>Sign up today</a>
+            </Translation>
           </div>
         </div>
       </Translation>
--- app/pages/pricing/pricing.js
+++ app/pages/pricing/pricing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Page from '../../components/page/page';
 import PricingEn from './pricing.en';
+import PricingEnGb from './pricing.en-GB';
 import PricingFr from './pricing.fr';
 
 export default class Pricing extends React.Component {
@@ -10,6 +11,7 @@ export default class Pricing extends React.Component {
     return (
       <Page>
         <PricingEn />
+        <PricingEnGb />
         <PricingFr />
       </Page>
     );
