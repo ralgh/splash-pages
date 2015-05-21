9aa64355f0acbdbb6bdd19cf40f3632e6bf5d399
Philip Harrison <philip@mailharrison.com>
2015-05-21 11:26:50 +0100

    Fix 404 pages

--- app/pages/not-found/not-found.js
+++ app/pages/not-found/not-found.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import Page from '../../components/page/page';
+import Message from '../../components/message/message';
 
 export default class NotFound extends React.Component {
   displayName = 'NotFound'
 
   render() {
     return (
-      <Page>
-        <h1>Not found</h1>
-      </Page>
+      <div className='not-found'>
+        <div className='not-found__container'>
+          <div className='not-found__container__center'>
+            <div className='not-found__container__center__inner'>
+              <div className='not-found__container__center__inner__container'>
+                <h1 className='not-found-header'><Message message='not_found.header' /></h1>
+                <p><Message message='not_found.explainer' /></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='not-found__footer'>
+          <div className='not-found-footer'>
+            <a href='https://gocardless.com' className='not-found-footer__link not-found-logo'>GoCardless</a>
+            <a href='https://twitter.com/gocardless' className='not-found-footer__link'>@gocardless</a>
+          </div>
+        </div>
+      </div>
     );
   }
 }
