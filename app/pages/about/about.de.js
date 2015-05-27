cdf66011c629394671420a997dfd84f7c96f2035
jutta-frieden <jutta.frieden@gmail.com>
2015-05-27 17:18:27 +0100

    First translation of about page

--- app/pages/about/about.de.js
+++ app/pages/about/about.de.js
@@ -9,36 +9,34 @@ export default class AboutDe extends React.Component {
       <Translation locales='de'>
         <div className='site-container u-padding-Vxxl'>
           <h2 className='u-text-heading u-color-heading u-text-l u-text-light u-padding-Txl'>
-            About GoCardless
+            Über GoCardless
           </h2>
           <div className='grid'>
             <div className='grid__cell u-size-2of3'>
               <p className='para'>
-                GoCardless makes it easy to accept recurring payments online.
-                We unify the world’s Direct Debit networks, simplify them, and open up access to everyone.
-                By doing so, we are creating an entirely new type of payment network.
+                GoCardless erlaubt es auf einfachste Art wiederkehrende Zahlungen online zu akzeptieren.
+                Wir bringen Lastschriftverfahren aus der ganzen Welt zusammen, vereinfachen sie, und schaffen Zugang.
+                Wir kreieren so ein innovatives und modernes Zahlungsnetzwerk.
               </p>
               <p className='para'>
-                Founded in 2011, we have quickly grown into one of Europe’s leading Direct Debit providers,
-                processing over half a billion pounds a year for over ten thousand businesses.
+                Seit 2011, unserem Gründungsjahr, sind wir schnell zu einem der führenden Lastschriftanbieter gewachsen,
+                und bearbeiten jährlich mehr als eine halbe Milliarde Euro für über zehntausend Organisationen.
               </p>
               <p className='para'>
-                We work with a huge range of organisations: small gyms and scout groups use us to reduce admin, fast growing companies
-                like Funding Circle use us to power their growth,
-                and established institutions like the Financial Times use us to dramatically reduce failure rates.
+                Wir arbeiten mit einer breitgefächerten Gruppe von Unternehmen: kleine Fitnessstudios und Pfadfindervereine nutzen uns, um ihren Verwaltungsaufwand zu minimieren, schnell wachsende Unternehmen 
+wie Funding Circle nutzen unser Produkt, um ihre schnelle Expansion zu unterstützen, und etablierte Firmen wie die Financial Times zählen auf GoCardless, um ihre Ausfallrate drastisch zu senken.
               </p>
               <p className='para'>
-                We are a small London based team, and we're backed by some of the world’s leading
-                investors including Balderton Capital, Accel Partners, and Y&nbsp;Combinator.
+                Wir sind ein junges Team in London, und werden unterstützt von Investoren, die zu den Besten der Welt gehören, u.a. Balderton Capital, Accel Partners, und Y Combinator.
               </p>
             </div>
             <div className='grid__cell u-size-1of3'>
               <div className='u-margin-Txxl'>
                 <img src='/images/press/publications-stacked.jpg' />
                 <p className='u-text-s u-color-p u-margin-Txxl'>
-                  For any press enquiries, please contact <a href='mailto:press@gocardless.com'>the GoCardless PR team</a>,
-                  or view our comprehensive <a href='https://www.dropbox.com/sh/sat8xsjntbf5mnh/AACn116IKyIWrNG7c8FnY0cYa?dl=0'>
-                    press pack
+                  Für Presseanfragen kontaktieren Sie bitte <a href='mailto:press@gocardless.com'>das GoCardless PR Team</a>,
+                  oder werfen Sie einen Blick auf unser umfangreiches <a href='https://www.dropbox.com/sh/sat8xsjntbf5mnh/AACn116IKyIWrNG7c8FnY0cYa?dl=0'>
+                    Presse Paket
                   </a>.
                 </p>
               </div>
